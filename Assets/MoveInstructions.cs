42bc9943af3033fddbd31c529a765782efd34cda
svc-student <svc-student@student.directory.jccc>
2018-05-10 17:50:28 -0500

    First commit

---
+++ Assets/MoveInstructions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveInstructions : MonoBehaviour {
+
+    public Vector3[] positions;
+    public float[] ranges;
+    public Vector3[] directions;
+
+    public float moveSpeed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+
+	}
+}
